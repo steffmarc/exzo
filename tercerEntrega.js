ff0d876ade14621a9df4dcb3c63df8a580695e22
estefaniamarcen <estefania.marcen@hotmail.com>
2024-07-05 15:32:37 -0300

    Se reparo el JS inicio y registro desde la nav

--- tercerEntrega.js
+++ tercerEntrega.js
@@ -1,222 +1,111 @@
 document.addEventListener("DOMContentLoaded", () => {
     let nuevoUsuario = JSON.parse(localStorage.getItem("nuevoUsuario")) || [];
 
-    const registroForm = () => {
-        const nombre = document.getElementById("nombreReg").value;
-        const contraseña = document.getElementById("passReg").value;
-        const edad = parseInt(document.getElementById("edad").value);
-        const localidad = document.getElementById("localidad").value;
-        const mensajeErrorNombre = document.getElementById("mensajeErrorNombre");
-        const mensajeErrorPass = document.getElementById("mensajeErrorPass");
-        const mensajeErrorEdad = document.getElementById("mensajeErrorEdad");
-        const mensajeErrorLocalidad = document.getElementById("mensajeErrorLocalidad");
 
-        if (nombre.trim() === '' || contraseña.trim() === '' || isNaN(edad) || localidad.trim() === '') {
-            mensajeErrorNombre.classList.add("invisible");
-            mensajeErrorPass.classList.add("invisible");
-            mensajeErrorEdad.classList.add("invisible");
-            mensajeErrorLocalidad.classList.add("invisible");
-            return;
-        }
-
-        let isValid = true;
-        if (nombre.length < 4) {
-            mensajeErrorNombre.classList.remove("invisible");
-            isValid = false;
-        } else {
-            mensajeErrorNombre.classList.add("invisible");
-        }
-
-        if (contraseña.length < 8) {
-            mensajeErrorPass.classList.remove("invisible");
-            isValid = false;
-        } else {
-            mensajeErrorPass.classList.add("invisible");
-        }
-
-        if (edad < 18) {
-            mensajeErrorEdad.classList.remove("invisible");
-            isValid = false;
-        } else {
-            mensajeErrorEdad.classList.add("invisible");
-        }
-
-        if (!localidad.match(/^[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]*$/)) {
-            mensajeErrorLocalidad.classList.remove("invisible");
-            isValid = false;
-        } else {
-            mensajeErrorLocalidad.classList.add("invisible");
-        }
-
-        if (!isValid) {
-            return;
-        }
-
-        nuevoUsuario.push({ nombre, contraseña, edad, localidad });
-        localStorage.setItem("nuevoUsuario", JSON.stringify(nuevoUsuario));
-        window.location.href = "./index.html";
-    };
-
-    const loginForm = () => {
+    const loginUsuario = () => {
         const nombre = document.getElementById("nombre").value;
         const contraseña = document.getElementById("pass").value;
+
+        const getDatos = JSON.parse(localStorage.getItem("nuevoUsuario")) || [];
+
+        const usuario = getDatos.find(usuario => usuario.nombre === nombre && usuario.contraseña === contraseña);
+
         const mensajeErrorUsuario = document.getElementById("mensajeErrorUsuario");
         const mensajeErrorCont = document.getElementById("mensajeErrorCont");
 
-        if (nombre.trim() === '' || contraseña.trim() === '') {
-            mensajeErrorUsuario.classList.add("invisible");
-            mensajeErrorCont.classList.add("invisible");
+        if (usuario) {
+            mostrarHome(usuario);
+        } else {
+            mostrarMensajeError(mensajeErrorUsuario, mensajeErrorCont);
+        }
+    };
+
+    const registroUsuarios = () => {
+        const nombre = document.getElementById("nombreReg").value;
+        const contraseña = document.getElementById("passReg").value;
+        const edad = document.getElementById("edad").value;
+        const localidad = document.getElementById("localidad").value;
+
+        const usuario = { nombre, contraseña, edad, localidad };
+        nuevoUsuario.push(usuario);
+
+        localStorage.setItem("nuevoUsuario", JSON.stringify(nuevoUsuario));
+        mostrarHome(usuario);
+    };
+
+    const mostrarHome = () => {
+        registroForm.style.display = "none";
+        loginForm.style.display = "none";
+        home.style.display = "block";
+        home.classList.add("d-flex");
+
+        let nav = document.getElementById("nav");
+        nav.innerHTML = `<nav id="nav">
+        <div class="logo">
+          <img src="../multimedia/logoMostaza.png" alt="Logo Exzo" />
+        </div>
+        <ul class="navLinks">
+          <li><a href="./index.html" id="navHome">Home</a></li>
+          <li><a href="./pages/informacion.html" id="navInfo">Información</a></li>
+          <li><a href="./pages/perfil.html" id="navPerfil">Perfil</a></li>
+        </ul>
+        <div>
+          <button class="btnInSesion" id="miCuenta">Mi Cuenta</button>
+          <button class="btnRegist" id="cerrarSesion">Cerrar Sesión</button>
+        </div>
+      </nav>`;
+    };
+
+    const mostrarMensajeError = (mensajeErrorUsuario, mensajeErrorCont) => {
+        const nombre = document.getElementById("nombre").value;
+        const contraseña = document.getElementById("pass").value;
+    
+        if (!nombre || !contraseña) {
             return;
         }
-
-        let usuario = nuevoUsuario.find(user => user.nombre === nombre);
-
-        if (usuario) {
-            if (usuario.contraseña === contraseña) {
-                window.location.href = "./index.html";
-            } else {
-                mensajeErrorCont.classList.remove("invisible");
-                mensajeErrorUsuario.classList.add("invisible");
-            }
-        } else {
-            mensajeErrorUsuario.classList.remove("invisible");
-            mensajeErrorCont.classList.add("invisible");
+    
+        const getDatos = JSON.parse(localStorage.getItem("nuevoUsuario")) || [];
+        const usuario = getDatos.find(usuario => usuario.nombre === nombre);
+    
+        if (!usuario) {
+            mensajeErrorUsuario.style.display = "block";
+            mensajeErrorCont.style.display = "none";
+        } else if (usuario.contraseña !== contraseña) {
+            mensajeErrorCont.style.display = "block";
+            mensajeErrorUsuario.style.display = "none";
         }
-
-        return false;
     };
 
-    const realizarCompra = (event) => {
+    let btnIniciarSesion = document.getElementById("btnIniciarSesion");
+    btnIniciarSesion.addEventListener("click", mostrarFormularioLogin = () => {
+        home.style.display = "none";
+        loginForm.style.display = "block";
+        registroForm.style.display = "none";
+    });
+
+    let btnRegist = document.getElementById("btnRegist");
+    btnRegist.addEventListener("click", mostrarFormularioLogin = () => {
+        home.style.display = "none";
+        registroForm.style.display = "block";
+        loginForm.style.display = "none";
+    });
+
+    document.getElementById("btnRegistro").addEventListener("click", (event) => {
         event.preventDefault();
+        mostrarFormularioRegistro();
+    });
 
-        const cantidad = parseFloat(document.getElementById("cantidadCompra").value);
-        const criptoSeleccionada = document.getElementById("criptoCompra").value;
-        const metodoPago = document.getElementById("metodoPagoCompra").value;
-
-        let cotizacion;
-        if (criptoSeleccionada === "bitcoin") {
-            cotizacion = 59337;
-        } else if (criptoSeleccionada === "ethereum") {
-            cotizacion = 3253;
-        } else if (criptoSeleccionada === "binance coin") {
-            cotizacion = 542.55;
-        } else if (criptoSeleccionada === "solana") {
-            cotizacion = 0.69;
-        } else if (criptoSeleccionada === "ripple") {
-            cotizacion = 0.446;
-        } else {
-            cotizacion = 0;
-        }
-
-        if (!isNaN(cantidad) && cantidad > 0 && cotizacion > 0) {
-            const total = cotizacion * cantidad;
-
-            document.getElementById("totalEstimadoCompra").textContent = `Total Estimado: $${total.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
-
-            const nuevoMovimiento = {
-                fecha: new Date().toLocaleString(),
-                tipo: 'Compra',
-                cantidad,
-                precioUnitario: cotizacion,
-                total,
-                metodoPago,
-                criptomoneda: criptoSeleccionada.charAt(0).toUpperCase() + criptoSeleccionada.slice(1)
-            };
-            mostrarCompraConfirmada(nuevoMovimiento);
-        } else {
-            console.error("Error: Cantidad inválida o cotización no definida.");
-        }
-    };
-
-    const mostrarCompraConfirmada = (detalleCompra) => {
-        const comprarContainer = document.getElementById("detalleCompra");
-
-        const innerHTMLContent = `
-            <h2>Compra Confirmada</h2>
-            <div>
-                <p><strong>Fecha:</strong> ${detalleCompra.fecha}</p>
-                <p><strong>Tipo:</strong> ${detalleCompra.tipo}</p>
-                <p><strong>Cantidad:</strong> ${detalleCompra.cantidad}</p>
-                <p><strong>Precio Unitario:</strong> $${detalleCompra.precioUnitario.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                <p><strong>Total:</strong> $${detalleCompra.total.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                <p><strong>Método de Pago:</strong> ${detalleCompra.metodoPago}</p>
-                <p><strong>Criptomoneda:</strong> ${detalleCompra.criptomoneda}</p>
-            </div>
-            <div>
-            <div id="btnComprobante">
-            <button id="btnVolver">Volver</button>
-            <button id="btnPerfil">Perfil</button>
-        </div>
-        </div>
-        `;
-
-        comprarContainer.innerHTML = innerHTMLContent;
-    };
-
-    const realizarVenta = (event) => {
+    document.getElementById("formRegistro").addEventListener("submit", (event) => {
         event.preventDefault();
-        const cantidad = parseFloat(document.getElementById("cantidadVenta").value);
-        const metodoPago = document.getElementById("metodoPagoVenta").value;
+        registroUsuarios();
+    });
 
-        let cotizacion;
-        const criptoSeleccionada = document.getElementById("criptoVenta").value;
+    document.getElementById("btnLogin").addEventListener("click", (event) => {
+        event.preventDefault();
+        loginUsuario();
+    });
 
-        if (criptoSeleccionada === "bitcoin") {
-            cotizacion = 59337;
-        } else if (criptoSeleccionada === "ethereum") {
-            cotizacion = 3253;
-        } else if (criptoSeleccionada === "binance coin") {
-            cotizacion = 3253;
-        } else {
-            cotizacion = 0;
-        }
+    
 
-        if (!isNaN(cantidad) && cantidad > 0 && cotizacion > 0) {
-            const total = cotizacion * cantidad;
 
-            document.getElementById("totalEstimadoVenta").textContent = `Total Estimado: $${total.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
-
-            const nuevoMovimiento = {
-                fecha: new Date().toLocaleString(),
-                tipo: 'Venta',
-                cantidad,
-                precioUnitario: cotizacion,
-                total,
-                metodoPago,
-                criptomoneda: criptoSeleccionada.charAt(0).toUpperCase() + criptoSeleccionada.slice(1)
-            };
-
-            mostrarVentaConfirmada(nuevoMovimiento);
-        } else {
-            console.error("Error: Cantidad inválida o cotización no definida.");
-        }
-    };
-
-    const mostrarVentaConfirmada = (detalleVenta) => {
-        const ventaContainer = document.getElementById("detalleVenta");
-
-        const innerHTMLContent = `
-            <h2>Venta Confirmada</h2>
-            <div>
-                <p><strong>Fecha:</strong> ${detalleVenta.fecha}</p>
-                <p><strong>Tipo:</strong> ${detalleVenta.tipo}</p>
-                <p><strong>Cantidad:</strong> ${detalleVenta.cantidad}</p>
-                <p><strong>Precio Unitario:</strong> $${detalleVenta.precioUnitario.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                <p><strong>Total:</strong> $${detalleVenta.total.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                <p><strong>Método de Pago:</strong> ${detalleVenta.metodoPago}</p>
-                <p><strong>Criptomoneda:</strong> ${detalleVenta.criptomoneda}</p>
-            </div>
-            <div id="btnComprobante">
-            <button id="btnVolver">Volver</button>
-            <button id="btnPerfil">Perfil</button>
-        </div>
-        `;
-
-        ventaContainer.innerHTML = innerHTMLContent;
-    };
-
-    document.getElementsByClassName("btnInSesion").addEventListener("click", loginForm);
-    document.getElementsByClassName("btnRegist").addEventListener("click", registroForm);
-    document.getElementById("formComprar").addEventListener("submit", realizarCompra);
-    document.getElementById("formVender").addEventListener("submit", realizarVenta);
-});
+    });
